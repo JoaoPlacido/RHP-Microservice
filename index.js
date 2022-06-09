9e199270e137659bacbb77f72a65b7c675362319
João Plácido da Silva Neto <37023372+JoaoPlacido@users.noreply.github.com>
2022-06-09 14:27:40 +0000

    make the RHP functionality

--- index.js
+++ index.js
@@ -10,11 +10,17 @@ app.use(express.static('public'));
 
 //load index page
 app.get('/',(req,res)=>{
-  res.sendfile(__dirname +'/view/index.html');
+  res.sendFile(__dirname +'/view/index.html');
 });
 
-
-
+//request header parse
+app.get('/api/whoami',(req,res)=>{
+  res.json({
+    ipaddress: req.headers['x-forwarded-for'],
+    language: req.headers["accept-language"],
+    software: req.headers['user-agent']
+  });
+});
 
 var listener = app.listen(3000, () => {
   console.log('Listenig on port ' + listener.address().port);
